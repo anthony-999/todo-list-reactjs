a4a8c14618b9a9d24eca6363f9f0c5f658799227
anthony-999 <anthonyvaldez99999@gmail.com>
2024-05-27 18:37:25 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import "./App.css";
+import { FaTrashAlt } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.css";
+
+function App() {
+  const [todos, setTodos] = useState([]); // State to store the list of todos
+  const [todoToDelete, setTodoToDelete] = useState(null); // State to store the index of the todo to be deleted
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // User input
+    const userInput = event.target.elements.todo.value;
+
+    if (userInput.trim()) {
+      // Add new todo to the list
+      setTodos([...todos, userInput]);
+      // Clear the input field
+      event.target.elements.todo.value = "";
+    }
+  };
+
+  const handleDelete = () => {
+    if (todoToDelete !== null) {
+      const newArray = todos.filter((item, index) => index !== todoToDelete);
+      setTodos(newArray);
+      setTodoToDelete(null);
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center mb-4 text-primary">Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group mb-3">
+          <input
+            className="form-control border border-primary"
+            type="text"
+            name="todo"
+            placeholder="Enter a new task"
+          />
+          <button className="btn btn-primary" type="submit">
+            Add
+          </button>
+        </div>
+      </form>
+
+      <div className="todo-list">
+        <ul className="list-group">
+          {todos.map((todo, index) => (
+            <li
+              key={index}
+              className="list-group-item d-flex justify-content-between align-items-center "
+            >
+              {todo}
+              <button
+                className="btn btn-danger"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+                onClick={() => setTodoToDelete(index)}
+              >
+                <FaTrashAlt />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Confirm Deletion
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              Are you sure you want to delete this todo?
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+                onClick={handleDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
